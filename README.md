f2456bf60bd2a2aa1cc53585c898c7164b23d1c5
David Hainzl <david.hainzl@seekda.com>
2019-02-26 23:23:32 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,34 @@
+# DHainzl TSLint Config
+
+> A [TSLint config](https://palantir.github.io/tslint/usage/tslint-json/) for my personal projects
+
+## Installation
+
+```sh
+npm install @dhainzl/ng-tslint-config --save
+```
+
+## Usage
+
+In `tslint.json`:
+
+```json
+{
+  "extends": "@dhainzl/ng-tslint-config"
+}
+```
+
+## Modification
+
+If you edit this lib, you need to manually update the version in the `package.json` and afterwards `npm publish` it yourself; There is no automated build here.
+
+Usually, I run the following commands (in this example updating to 6.1.1):
+
+```
+<Change version to 6.1.1 in package.json>
+git commit -am <commit msg>
+git push
+git tag 6.1.1
+git push origin 6.1.1
+npm publish
+```
---
+++ tslint.js
@@ -0,0 +1,139 @@
+const path = require('path');
+const resolvePkg = require('resolve-pkg');
+
+module.exports = {
+    "rulesDirectory": [
+        path.join(path.dirname(require.resolve('tslint')), 'rules'),
+        path.join(path.dirname(require.resolve('codelyzer')), './'),
+        path.join(path.dirname(require.resolve('tslint-eslint-rules')), 'dist/rules'),
+        path.join(resolvePkg('tslint-jasmine-rules'), 'dist'),
+    ],
+    "rules": {
+        "ter-indent": [true, 4, { "SwitchCase": 1 }],
+        "prefer-const": true,
+        "array-bracket-spacing": [true, "always"],
+        "trailing-comma": [true, {multiline: 'always', singleline: 'never'}],
+        "class-name": true,
+        "comment-format": [
+            true,
+            "check-space",
+        ],
+        "curly": [ true, "ignore-same-line" ],
+        "eofline": true,
+        "forin": true,
+        "indent": [
+            true,
+            "spaces",
+            4
+        ],
+        "label-position": true,
+        "max-line-length": [
+            true,
+            140,
+        ],
+        "member-access": false,
+        "member-ordering": [
+            true,
+            "static-before-instance",
+            "variables-before-functions",
+        ],
+        "no-arg": true,
+        "no-bitwise": true,
+        "no-console": [
+            true,
+            "debug",
+            "info",
+            "time",
+            "timeEnd",
+            "trace",
+        ],
+        "no-construct": true,
+        "no-debugger": true,
+        "no-duplicate-variable": true,
+        "no-empty": false,
+        "no-eval": true,
+        "no-inferrable-types": false,
+        "no-shadowed-variable": true,
+        "no-string-literal": true,
+        "no-switch-case-fall-through": true,
+        "no-trailing-whitespace": true,
+        "no-unused-variable": true,
+        "no-unused-expression": true,
+        "no-use-before-declare": true,
+        "no-var-keyword": true,
+        "object-literal-sort-keys": false,
+        "one-line": [
+            true,
+            "check-open-brace",
+            "check-catch",
+            "check-else",
+            "check-whitespace",
+        ],
+        "quotemark": [
+            true,
+            "single",
+        ],
+        "radix": true,
+        "semicolon": [
+            true,
+            "always",
+        ],
+        "triple-equals": [
+            true,
+            "allow-null-check",
+        ],
+        "typedef-whitespace": [
+            true,
+            {
+                "call-signature": "nospace",
+                "index-signature": "nospace",
+                "parameter": "nospace",
+                "property-declaration": "nospace",
+                "variable-declaration": "nospace",
+            },
+        ],
+        "variable-name": false,
+        "whitespace": [
+            true,
+            "check-branch",
+            "check-decl",
+            "check-operator",
+            "check-separator",
+            "check-type",
+        ],
+
+        "use-input-property-decorator": true,
+        "use-output-property-decorator": true,
+        "use-host-property-decorator": true,
+        "no-input-rename": true,
+        "no-output-rename": true,
+        "use-life-cycle-interface": true,
+        "use-pipe-transform-interface": true,
+        "component-class-suffix": true,
+        "directive-class-suffix": true,
+        "callable-types": true,
+        "import-spacing": true,
+        "interface-over-type-literal": true,
+        "no-empty-interface": true,
+        "no-string-throw": true,
+        "unified-signatures": true,
+        "no-focused-tests": true,
+        "arrow-return-shorthand": true,
+        "no-consecutive-blank-lines": true,
+        "ordered-imports": {
+            "options": {
+                "grouped-imports": true
+            }
+        },
+        "deprecation": {
+            "severity": "warning"
+        },
+        "no-any": {
+            "severity": "warning"
+        },
+        "typedef": {
+            "options": [ "call-signature", "parameter" ],
+            "severity": "warning"
+        }
+    },
+}
